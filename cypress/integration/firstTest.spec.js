01003e6fc1201f0d335c338be7e9fb87874d10b9
Eugenia Cherevko <eugenia.cherevko@idealofsweden.com>
2022-02-10 17:56:07 +0100

    Practise with contains and should

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,2 +1,46 @@
 ///<reference types="Cypress" />
 
+it('SHOULD', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.get('[data-qa-node="amount"]')
+        .type(100)
+        .should('have.value', 100)
+        .and('be.visible')    
+})
+
+it('EXPECT', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.get('[data-qa-node="amount"]')
+        .type(100).then(input => {
+            expect(input).to.have.value(100)
+        })
+})
+
+it('check default value for deposit', ()=> {
+    cy.visit('https://next.privat24.ua/deposit?lang=en')
+    cy.get('[data-qa-value="UAH"]')
+        .should('be.checked')
+}) 
+
+it('check with visible archiv deposit', ()=> {
+    cy.visit('https://next.privat24.ua/deposit?lang=en')
+    cy.contains('Мої депозити')
+        .trigger('mouseover')
+        .get('#archiveDeposits')
+        .should('be.visible')
+}) 
+
+it('check correct attr in button', ()=> {
+    cy.viewport(1800, 700)
+    cy.visit('https://next.privat24.ua?lang=en')
+    cy.contains('Show cards')
+        .should('have.attr', 'type')
+        .and('match', /button/)
+}) 
+
+
+it.only('check correct url', ()=> {
+    cy.visit('https://next.privat24.ua?lang=en')
+    cy.url()
+        .should('eq', 'https://next.privat24.ua/?lang=en')
+}) 
