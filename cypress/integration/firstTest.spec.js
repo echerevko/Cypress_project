038a835647fd65dc45524c3368dfa5507814d2be
Eugenia Cherevko <eugenia.cherevko@idealofsweden.com>
2022-02-10 15:47:52 +0100

    Practice with containce

--- cypress/integration/firstTest.spec.js
+++ cypress/integration/firstTest.spec.js
@@ -1,2 +1,21 @@
 ///<reference types="Cypress" />
 
+it('', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('Sign in')
+})
+
+it('', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('div','Sign in')
+})
+
+it('', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.contains('SIGN in', {matchCase:false})
+})
+
+it.only('', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.get('section').contains('Add to cart', {matchCase:false})
+})
